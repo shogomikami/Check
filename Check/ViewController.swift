58096d414e4d41200f9be8819f8945b66b4aa029
mikamishogo <sho.mikami.7@gmail.com>
2020-05-24 10:43:38 +0900

    Initial Commit

---
+++ Check/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Check
+//
+//  Created by 三上翔吾 on 2020/05/24.
+//  Copyright © 2020 Mikami Sho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
